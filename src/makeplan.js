9bc87208cfb96981a0c337f38bb62a1d0b379c01
Mike Garrity <mgarrity@mikegslptop2495.lan>
2020-06-27 13:40:37 -0400

    First makeplan test passes

--- src/makeplan.js
+++ src/makeplan.js
@@ -6,15 +6,94 @@ var u = require('../src/util');
 function generate_runs(pts, heights) {
     let result = [];
 
-    let i0 = intersect_line(pts[0], pts[1], heights);
-    let i1 = intersect_line(pts[1], pts[2], heights);
-    let i2 = intersect_line(pts[2], pts[3], heights);
-    let i3 = intersect_line(pts[3], pts[4], heights);
-    let i4 = intersect_line(pts[4], pts[5], heights);
+    let run = [];
+    for (let j=0; j<heights.length-1; j++) {
+        run[j] = undefined;
+    }
 
-    result[0] = [[i0[0], i0[1], i4[1], i4[0]]];
-    result[1] = [[i0[1], i0[2], i1[2], pts[2], i2[2], i3[2], pts[4], i4[1]]];
-    result[2] = [[i0[2], pts[1], i1[2]], [i2[2], pts[3], i3[2]]];
+    for (let i=1; i<pts.length; i++) {
+//        console.log("process line from pts[" + (i-1) + "] to pts[" + i + "]");
+
+        // intersect line from i-1 to i against all heights
+        let li = intersect_line(pts[i-1], pts[i], heights);
+
+        let intersects = [];
+        for (let j=0; j<heights.length; j++) {
+            intersects[j] = (li[j] != null) ? " 1 " : " 0 ";
+        }
+//        console.log("intersects = " + intersects);
+
+        // loop over heights
+        for (let j=0; j<heights.length-1; j++) {
+
+            // if line doesn't intersect heights[i], then ...
+            if (li[j] == null) {
+                // if we're in a run, and end point is between heights, add the end point
+                if (run[j] && pts[i][2] > heights[j] && pts[i][2] <= heights[j+1]) {
+                    if (li[j+1] != null) {
+//                        console.log("run[" + j + "] - push li[" + (j+1) + "] A");
+                        run[j].push(li[j+1]);
+                    }
+//                    console.log("run[" + j + "] - push pts[" + i + "] B");
+                    run[j].push(pts[i]);
+                }
+                else if (li[j+1] != null) {
+//                    console.log("run[" + j + "] - push li[" + (j+1) + "] C");
+                    run[j].push(li[j+1]);
+                }
+                // otherwise do nothing
+                else {
+
+                }
+            }
+            // if line does intersect heights[i], then ...
+            else {
+                // if we're in a run, then ...
+                if (run[j]) {
+                    // push the intersection point
+                    if (li[j+1] != null) {
+//                        console.log("run[" + j + "] - push li[" + (j+1) + "] D");
+                        run[j].push(li[j+1]);
+                    }
+//                    console.log("run[" + j + "] - push li[" + j + "] E");
+                    run[j].push(li[j]);
+
+                    // move the run onto the results
+//                    console.log("run[" + j + "] - move to results");
+                    if (result[j]) {
+                        result[j].push(run[j]);
+                    } else {
+                        result[j] = [run[j]];
+                    }
+
+                    run[j] = undefined;
+                }
+                // otherwise, start a run with the intersection point
+                else {
+//                    console.log("run[" + j + "] - start with li[" + j + "] F");
+                    run[j] = [li[j]];
+                    // if we're in a run, and end point is between heights, add the end point
+                    if (run[j] && pts[i][2] > heights[j] && pts[i][2] <= heights[j+1]) {
+//                        console.log("run[" + j + "] - push pts[" + i + "] G");
+                        run[j].push(pts[i]);
+                    }
+                    else if (li[j+1] != null) {
+//                        console.log("run[" + j + "] - push li[" + (j+1) + "] H");
+                        run[j].push(li[j+1]);
+                    }
+                }
+            }
+        }
+    }
+
+    for (let k=0; k<result.length; k++) {
+//        console.log("result[" + k + "].length = " + result[k].length);
+        if (result[k]) {
+            for (let l=0; l<result[k].length; l++) {
+//                console.log("result[" + k + "][" + l + "].length = " + result[k][l].length);
+            }
+        }
+    }
 
     return result;
 }
